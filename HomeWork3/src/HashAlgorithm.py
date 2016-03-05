84c27e54700577fe23b9857cfa7864b7c60923d5
Carlos Luna <Carlos Luna>
2016-03-05 07:12:37 -0700

    Homework 3

---
+++ HomeWork3/src/HashAlgorithm.py
@@ -0,0 +1,31 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+main = ('__HashAlgorithm__' == __name__)
+
+__author__ = "Carlos Luna"
+__date__ = "$Mar 4, 2016 5:31:23 AM$"
+
+#Function hash takes in string and produces hash value
+def hash_new(key):
+#initialize variable that will return ascii
+    a = 1
+#Step through passed in string value 
+#take ascii variable and multiply by 'prime' 61
+    for i in str(key):
+        a = a * (ord(i) * 61)
+    #Return ascii variable and perform bitwise AND operation cast as int
+    return int(a & 0x7fffffff)
+        
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HomeWork3/src/testhashmap.py
@@ -0,0 +1,45 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+import HashMap
+__author__ = "Carlos Luna"
+__date__ = "$Mar 4, 2016 6:20:08 AM$"
+
+states = HashMap.new()
+HashMap.set(states, 'Oregon', 'OR')
+HashMap.set(states, 'Florida', 'FL')
+HashMap.set(states, 'California', 'CA')
+
+cities = HashMap.new()
+HashMap.set(cities, 'CA', 'San Francisco')
+HashMap.set(cities, 'MI', 'Detroit')
+HashMap.set(cities, 'FL', 'Jacksonville')
+
+HashMap.set(cities, 'NY', 'New York')
+HashMap.set(cities, 'OR', 'Portland')
+
+print ('-' * 10)
+print ("NY states has: %s" % HashMap.get(cities, 'NY'))
+print ("OR States has: %s" % HashMap.get(cities, 'OR'))
+
+
+print('-' * 10)
+print("Oregon's abbreviation is: %s" % HashMap.get(states, 'Oregon'))
+print("Florida's abreviation is: %s" % HashMap.get(states, 'Florida'))
+
+print('-' * 10)
+print("Oregan has %s" % HashMap.get(cities, HashMap.get(states, 'Oregon')))
+print("California has %s" % HashMap.get(cities, HashMap.get(states, 'California')))
+
+print('-' * 10)
+HashMap.list(states)
+
+print('-'* 10)
+HashMap.list(cities)
+
+print('-'* 10)
+state = HashMap.get(states, 'Texas')
+
+if not state: 
+    print("Sorry, no Texas")
+
