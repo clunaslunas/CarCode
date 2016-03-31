58942232510398cc1f24d528a1dcfcea82e1f90c
Carlos Luna <Carlos Luna>
2016-03-31 17:20:05 -0600

    Homework branch 1

---
+++ homework4/src/ReadInXML.py
@@ -0,0 +1,33 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+import xml.etree.ElementTree as ET
+
+__author__ = "Carlos"
+__date__ = "$Mar 30, 2016 9:45:52 AM$"
+
+tree = ET.parse('graph.xml')
+root = tree.getroot()
+root.tag
+root.attrib
+
+for child in root:
+    print(child.tag, child.attrib)
+x = root[0][0].attrib
+y = root[0][0].tag
+
+#Prints out a string you must read in as a dictionary with key value pairs
+print('Attribute is: ', x)
+
+print('Tag is: ', y)
+
+parser = ET.XMLPullParser(['start', 'end'])
+parser.feed('<mytag>sometext')
+list(parser.read_events())
+print('The parser is: ', parser)
+parser.feed('more text</mytag>')
+for event, elem in parser.read_events():
+    print(event)
+    print(elem.tag, 'text= ', elem.text)
+
