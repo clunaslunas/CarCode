375e7c3274ac48bfbe419b552961d38924027364
clunaslunas <rclunas09@yahoo.com>
2016-04-20 05:34:43 -0600

    0530

--- netbeansprojects/parseAgainXML/src/parseagainxml.py
+++ netbeansprojects/parseAgainXML/src/parseagainxml.py
@@ -2,7 +2,7 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor
 from xml.dom import minidom
-
+from collections import defaultdict
 __author__ = "Carlos"
 __date__ = "$Apr 18, 2016 7:50:21 AM$"
 
@@ -10,55 +10,93 @@ xmlDoc = minidom.parse("graph.xml")
 vertices = xmlDoc.getElementsByTagName("Vertices")[0]
 vertex = vertices.getElementsByTagName("Vertex")
 #print(type(vertex))
-def makeVertDict(nodeList):
-    vertLabVal = {}    
-    for verts in vertex:
-        vertIdVal = int(verts.attributes["vertexId"].value)
-        labelVal = verts.attributes["label"].value
-
-        #print ("Vertex ID:", int(vertIdVal), "Label: ", labelVal)
-        #print (verts)
-        
-        vertLabVal[labelVal] = vertIdVal
-        #print(vertLabVal)
-    return vertLabVal
-        
-
-vertDict = makeVertDict(vertex)
 
 '''Pull my edges and store list in an array Edge[Head] = (tail, cost): tuple'''
 edges = xmlDoc.getElementsByTagName("Edges")[0]
-edge = edges.getElementsByTagName("Edge")
+edgeObj = edges.getElementsByTagName("Edge")
 #print(edge)
 
-def makeEdgeDict(node):
-    edgeDict = {}
-    for edges in edge:
+class Vertex:
+    def __init__(self, vertexId, label):
+        self.vertexId = vertexId
+        self.label = label
+        self.adjacent = []
+        self.previous = None
+        
+class Edge:
+    def __init__(self, v1, v2, weight = 0):
+        self.v1 = v1
+        self.v2 = v2
+        self.weight = weight
+    
+    def __lt__(self,other):
+        return self.weight < other.weight
+    
+
+def makeVertList(nodeList):
+    vertLabVal = {}    
+    listOfVerts = []
+    for verts in vertex:
+        vertIdVal = int(verts.attributes["vertexId"].value)
+        labelVal = verts.attributes["label"].value
+#        listOfVerts = Vertex(vertIdVal, labelVal)
+#        print (listOfVerts.vertexId, listOfVerts.label)
+
+        #print ("Vertex ID:", int(vertIdVal), "Label: ", labelVal)
+        #print (verts)
+        vertLabVal[labelVal] = vertIdVal
+        listOfVerts.append(Vertex(vertIdVal, labelVal))
+        #print(vertLabVal)
+    return vertLabVal
+
+
+
+
+def makeEdgeList(node):
+    edgeDictT = {}
+    edgeDictH = {}
+    listOfEdges = []
+    for edges in edgeObj:
         tail = int(edges.attributes["tail"].value)
         head = int(edges.attributes["head"].value)
         weight = float(edges.attributes["weight"].value)
+        listOfEdges.append(Edge(tail, head, weight))
         #print ("Tail: ", tail, "Head: ", head, "Weight", weight)    
-        edgeDict[tail] = ((head, weight))
-    return edgeDict
+        #edgeDict[tail] = ((head, weight))
+        edgeDictT.setdefault(tail, []).append((head, weight))
+        edgeDictH.setdefault(head, []).append((tail, weight))
+#        print(listOfEdges.v1, listOfEdges.v2, listOfEdges.weight)
+    return edgeDictT
 
-edgeDict = makeEdgeDict(edge)
 
-graph = (vertDict, edgeDict)
-print(graph)
-#for edges in edge:
-#    tail = int(edges.attributes["tail"].value)
-#    head = int(edges.attributes["head"].value)
-#    weight = float(edges.attributes["weight"].value)
-#    print ("Tail: ", tail, "Head: ", head, "Weight", weight)
-#    edgeDict = {}
-#    edgeDict[tail] = ((head, weight))
-#    print (edgeDict)
-
+edges = makeEdgeList(edgeObj)
+x = []
+for items in edges:
+    x.append(edges.values())
+    if (20 , 4.61) in x:
+        x.index((20 , 4.61))
+        print("something")
+print(x)
     
-#def fillDict (label, id):
-#    vertLabVal = {}    
-#    for i in vertLabVal:
-#        vertLabVal[label] = id
-#    return vertLabVal
-#fillDict(labelVal, vertIdVal)    
-#print (fillDict)
+#print (edges)
+#vertices = makeVertList(vertex)
+#for k in vertices:
+#    #print ("Label: ", k)
+#    for tails in edges:
+#        if vertices[k] == tails:
+#            #print("Tail: ", tails)
+#            #print (edges[tails])
+#            x = edges[tails].values()
+#            if (29, 7.1) in x:
+#                print("found at ", edges[tails])
+#                break
+#            else:
+#                print("not found")
+
+            
+    
+#for edge in edges:
+#    print(edge.v1, edge.v2, edge.weight)
+
+
+
